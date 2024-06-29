9db1e81576d2a15f541502dde0afde15d1329282
HamdiHarahap <hamdiharahap2005@gmail.com>
2024-06-29 12:24:44 +0700

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,21 @@ feather.replace();
 const questions = document.querySelectorAll('.question');
 
 questions.forEach(question => {
+    const index = question.getAttribute('data-index');
+    const hover = document.querySelector(`.question[data-index='${index}']`)
+
+    question.addEventListener('mouseenter', function() {
+        hover.style.backgroundColor = '#414141'
+    })
+
+    question.addEventListener('mouseleave', function() {
+        hover.style.backgroundColor = '#2d2d2d'
+    })
+
     question.addEventListener('click', function() {
-        const index = this.getAttribute('data-index');
-        const answer = document.querySelector(`.answer[data-index="${index}"]`);
-        const openIcon = document.querySelector(`.open[data-index="${index}"]`);
-        const closeIcon = document.querySelector(`.x[data-index="${index}"]`);
+        const answer = document.querySelector(`.answer[data-index='${index}']`);
+        const openIcon = document.querySelector(`.open[data-index='${index}']`);
+        const closeIcon = document.querySelector(`.x[data-index='${index}']`);
 
         answer.classList.toggle('alive');
         openIcon.classList.toggle('close');
